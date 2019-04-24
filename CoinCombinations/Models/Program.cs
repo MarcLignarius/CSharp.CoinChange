13e6fa2d55bd28a04df328b05d8da1bea8945cee
Jared Farkas and Marc Davies <marc@>
2019-04-24 13:53:11 -0700

    add UI functionality

--- CoinCombinations/Models/Program.cs
+++ CoinCombinations/Models/Program.cs
@@ -6,9 +6,14 @@ namespace CoinCombinations.Models
   public class Program
   {
 
-     public static void Main()
-     {
-        
-     }
+    public static void Main()
+    {
+
+      Console.WriteLine("How many cents do you have?");
+      int stringUserInput = int.Parse(Console.ReadLine());
+      ChangeMake newChangeMake = new ChangeMake(stringUserInput);
+      Console.WriteLine(newChangeMake.GiveChange());
+
+    }
   }
 }
